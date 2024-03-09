5534ac70363d7bd62b4bfb87d9b107732d934f69
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-03-09 22:05:29 +0500

    asdasd

--- config/settings.py
+++ config/settings.py
@@ -99,5 +99,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "static_files"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
