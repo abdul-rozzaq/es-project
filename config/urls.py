9d8888b42dbb4f9554c6646bc48357b74b919a73
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-03-09 22:09:52 +0500

    static

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 
 from project.views import home_page
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home_page'),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
